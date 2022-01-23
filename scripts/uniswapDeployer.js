19ae3b567c8f3b56532b09a41e012c9b90c03137
Lazar Jankovic <jlazar997@gmail.com>
2022-01-23 16:38:24 +0100

    deploy tokens

--- scripts/uniswapDeployer.js
+++ scripts/uniswapDeployer.js
@@ -2,16 +2,34 @@ const { ethers } = require('hardhat');
 
 async function main() {
 	const [owner] = await ethers.getSigners();
+	const { chainId } = await owner.provider.getNetwork();
 
 	const Factory = await ethers.getContractFactory('UniswapV2Factory');
 	const PrimaryToken = await ethers.getContractFactory('PrimaryToken');
 	const SecondaryToken = await ethers.getContractFactory('SecondaryToken');
 
+	// deploy only to hardhat local net or polygon mumbai testnet
+	if (chainId !== 31337 || chainId !== 80001) console.log('wrong network');
+
 	const factory = await Factory.deploy(owner.address);
 
 	await factory.deployed();
-	await factory.createPair();
 	console.log('Factory deployed to:', factory.address);
+
+	const primaryToken = await PrimaryToken.deploy();
+	const secondaryToken = await SecondaryToken.deploy();
+
+	await primaryToken.deployed();
+	await secondaryToken.deployed();
+
+	const [primaryAddress, secondaryAddress] = [
+		primaryToken.address,
+		secondaryToken.address,
+	];
+
+	console.log('Tokens deployed to:', primaryAddress, secondaryAddress);
+
+	await factory.createPair(primaryAddress, secondaryAddress);
 }
 
 main()
